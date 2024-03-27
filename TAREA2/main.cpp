efb9eb0bffd616d23e38d84aebb5daa6166a3d5b
Fidel-DevDyscovery <162225999+Fidel-DevDyscovery@users.noreply.github.com>
2024-03-28 09:07:25 +1200

    ejercicio 1

--- TAREA2/main.cpp
+++ TAREA2/main.cpp
@@ -2,25 +2,22 @@
 
 using namespace std;
 
-int main() {
-    int sumatoria = 0;
-
-    for (int num = 2; num <= 100; num++) {
-        bool primo = true;
-
-        for (int i = 2; i <= num / 2; i++) {
-            if (num % i == 0) {
-                primo = false;
-                break;
-            }
-        }
-
-        if (primo) {
-            cout << num << " es primo" << endl;
-            sumatoria += num;
-        }
+int main()
+{
+    int numero;
+    int suma;
+    int multiplicacion;
+    double promedio;
+    for(int i=1; i<=5; i++){
+        cout<<"INGRESE UN NUMERO ENTERO POSITIVO"<<endl;
+        cin>>numero;
+        suma=suma+numero;
+        multiplicacion=multiplicacion*numero;
     }
+    promedio=suma/5;
+    cout<<"La suma de los 5 numeros es:"<<suma<<endl;
+    cout<<"La multiplicacion de los 5 numeros es:"<<multiplicacion<<endl;
+    cout<<"El promedio de los 5 numeros es:"<< promedio<<endl;
 
-    cout << "La sumatoria de los numeros primos es: " << sumatoria << endl;
     return 0;
 }
